6f317ca3915ca0918df53bf865a9963a429bc146
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-08-07 02:45:55 -0600

    Add Scaffold to the app.

--- app/src/main/java/com/darssolutions/wellnessapp/ui/WellnessApp.kt
+++ app/src/main/java/com/darssolutions/wellnessapp/ui/WellnessApp.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.darssolutions.wellnessapp.ui.screen.WellnessScreen
 
 @Composable
