6e01cdc129134b22f0c42fcf8b4ec17d742a6835
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-08-06 19:04:13 -0600

    Change UI with State.

--- app/src/main/java/com/darssolutions/wellnessapp/ui/components/WaterCounter.kt
+++ app/src/main/java/com/darssolutions/wellnessapp/ui/components/WaterCounter.kt
@@ -3,11 +3,11 @@ package com.darssolutions.wellnessapp.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -19,14 +19,18 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableIntStateOf(0) }
 
     Column(modifier = modifier.padding(16.dp)) {
-        Text(
-            text = "You've had $count glasses of water today",
-            modifier = Modifier
-                .padding(16.dp),
-        )
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses of water today",
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier
+                    .padding(16.dp),
+            )
+        }
         Button(
             onClick = { count++ },
-            Modifier.padding(top = 8.dp)
+            enabled = count < 10,
+            modifier = Modifier.padding(top = 8.dp)
         ) {
             Text("Add one")
         }
