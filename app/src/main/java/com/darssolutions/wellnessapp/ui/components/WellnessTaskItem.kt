523e30baf4b85b4041132a12fc2e5950e9ef1420
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-08-07 01:00:17 -0600

    Implements Stateful Components.

--- app/src/main/java/com/darssolutions/wellnessapp/ui/components/WellnessTaskItem.kt
+++ app/src/main/java/com/darssolutions/wellnessapp/ui/components/WellnessTaskItem.kt
@@ -4,18 +4,38 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WellnessTaskItem(
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier){
+    var checkedState by rememberSaveable { mutableStateOf(false)}
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // TODO: we will implement this later!
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -25,11 +45,15 @@ fun WellnessTaskItem(
         Text(
             modifier = Modifier
                 .weight(1f)
-                .padding(start = 16.dp),
-            text = taskName
+                .padding(start = 16.dp), text = taskName
+        )
+        Checkbox(
+            checked = checked, onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
-            Icon(Icons.Filled.Close, contentDescription = "Close")
+            Icon(
+                Icons.Filled.Close, contentDescription = "Close"
+            )
         }
     }
 }
@@ -37,5 +61,5 @@ fun WellnessTaskItem(
 @Preview(showBackground = true)
 @Composable
 fun WellnessTaskItemPreview() {
-    WellnessTaskItem("Task 1", onClose = {})
+    WellnessTaskItem("Task 1", modifier = Modifier)
 }
